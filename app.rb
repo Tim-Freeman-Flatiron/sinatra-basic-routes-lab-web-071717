65d48da21953978b1237be6802a530dab0f8b52d
Tim Freeman <d.timothy.freeman@gmail.com>
2017-08-07 11:55:11 -0400

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/name' do
+		"My name is Tim."
+	end
+
+	get '/hometown' do
+		"My hometown is Gallatin, TN."
+	end
+
+	get '/favorite-song' do
+		"My favorite song is Grey Street."
+	end
 end
